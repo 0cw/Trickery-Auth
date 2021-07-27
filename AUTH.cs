8b369419dc876e19100d22afa77be35943adbd74
lambwheit <38606611+lambwheit@users.noreply.github.com>
2021-07-27 23:32:05 +0800

    Update AUTH.cs
    Removed Leaf.xnet Dependency  + removed redundant code

--- AUTH.cs
+++ AUTH.cs
@@ -1,131 +1,67 @@
-﻿using Leaf.xNet;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TrickeryAUTH
 {
     public class AUTH
     {
-        private static string AuthResponse = "";
-        //poopcode
-        public static bool Login(string AuthKey)
+        private static string Parse(string source, string left, string right) => source.Split(new string[1] { left }, StringSplitOptions.None)[1].Split(new string[1] { right }, StringSplitOptions.None)[0];
+        private static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
-            using (var request = new HttpRequest())
-            {
-                bool response;
-                string get = request.Get("https://trickery.to/forum/tapi/index.php?users/me&oauth_token=" + AuthKey).ToString();
-                if (get.Contains("user"))
-                {
-                    AuthResponse = get;
-                    response = true;
-                }
-                return response;
-            }
-        }
-
-        public static string GetRawJSON(string AuthKey)
-        {
-            bool repsonse = Login(AuthKey);
-            if (Login(AuthKey))
-            {
-                return AuthResponse;
-            }
-            else
-            {
-                return "Invalid Key";
-            }
-        }
-
-        public static string GetUsername(string AuthKey)
-        {
-            bool repsonse = Login(AuthKey);
-            if (repsonse == true)
-            {
-                using (var request = new HttpRequest())
-                {
-                    string Username = Parse(AuthResponse, "\"username\": \"", "\",");
-                    return Username;
-                }
-            }
-            else
-            {
-                return "Invalid Key";
-            }
-        }
-
-        public static string GetUserID(string AuthKey)
-        {
-            bool repsonse = Login(AuthKey);
-            if (repsonse == true)
-            {
-                using (var request = new HttpRequest())
-                {
-                    string UID = Parse(AuthResponse, "\"user_id\": ", ",");
-                    return UID;
-                }
-            }
-            else
-            {
-                return "Invalid Key";
-            }
-        }
-
-        public static string GetUserEmail(string AuthKey)
-        {
-            bool repsonse = Login(AuthKey);
-            if (repsonse == true)
-            {
-                using (var request = new HttpRequest())
-                {
-                    string UID = Parse(AuthResponse, "\"user_email\": \"", "\",");
-                    return UID;
-                }
-            }
-            else
-            {
-                return "Invalid Key";
-            }
-        }
-        public static string GetRegistrationDate(string AuthKey)
-        {
-            bool repsonse = Login(AuthKey);
-            if (repsonse == true)
-            {
-                using (var request = new HttpRequest())
-                {
-                    string timestampstring = Parse(AuthResponse, "\"user_register_date\": ", ",");
-                    double timestamp = double.Parse(timestampstring);
-                    DateTime time = UnixTimeStampToDateTime(timestamp);
-                    string time2 = time.ToString("dd/MM/yyyy");
-                    return time2;
-                }
-            }
-            else
-            {
-                return "Invalid Key";
-            }
-        }
-
-
-        private static string Parse(string source, string left, string right) => source.Split(new string[1]
-        {
-                      left
-        }, StringSplitOptions.None)[1].Split(new string[1]
-        {
-                      right
-        }, StringSplitOptions.None)[0];
-
-        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
-        {
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
             dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
             return dtDateTime;
         }
+        private static string AuthResponse = "";
+        public static bool Login(string AuthKey)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://trickery.to/forum/tapi/index.php?users/me&oauth_token=" + AuthKey);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            AuthResponse = responseString;
+            if (AuthResponse.Contains("user"))
+                return true;
+            return false;
+        }
+        public static string GetRawJSON(string AuthKey)
+        {
+            if (Login(AuthKey))
+                return AuthResponse;
+            else
+                return "Invalid Key";
+        }
+        public static string GetUsername(string AuthKey)
+        {
+            if (Login(AuthKey))
+                return Parse(AuthResponse, "\"username\": \"", "\",");
+            else
+                return "Invalid Key";
+        }
+        public static string GetUserID(string AuthKey)
+        {
+            if (Login(AuthKey))
+                return Parse(AuthResponse, "\"user_id\": ", ",");
+            else
+                return "Invalid Key";
 
+        }
+        public static string GetUserEmail(string AuthKey)
+        {
+            if (Login(AuthKey))
 
+                return Parse(AuthResponse, "\"user_email\": \"", "\",");
+            else
+                return "Invalid Key";
+
+        }
+        public static string GetRegistrationDate(string AuthKey)
+        {
+            if (Login(AuthKey))
+                return (UnixTimeStampToDateTime(double.Parse(Parse(AuthResponse, "\"user_register_date\": ", ",")))).ToString("dd/MM/yyyy");
+            else
+                return "Invalid Key";
+        }
     }
+
 }
